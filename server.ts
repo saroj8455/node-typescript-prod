c63a5e7433b9c495c82fc4ec99e279997a913c2e
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-12-23 18:40:32 +0530

    implement db connection

--- server.ts
+++ server.ts
@@ -1,6 +1,24 @@
+import * as dotenv from 'dotenv';
 import app from './app';
+import mongoose from 'mongoose';
+
+// Configuration ENV
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
+
+const connectToDB = async () => {
+  try {
+    // Connect to MongoDB database
+    const connectionUrl = process.env.REMOTEURI ? process.env.REMOTEURI : '';
+    const conn = await mongoose.connect(connectionUrl);
+    console.log(`Connected to Remote Host : ${conn.connection.host}`);
+  } catch (error) {
+    console.log('Unable to Connect Remote DB', error);
+  }
+};
+
+connectToDB();
 
 app.get('/', (req, res) => res.send('Hello from server!'));
 
