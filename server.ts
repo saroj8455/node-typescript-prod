68847b174052a6ef2059794d9cd9360114b0dc8e
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-12-23 21:30:57 +0530

    update homepage api to ency message

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 import * as dotenv from 'dotenv';
 import app from './app';
 import mongoose from 'mongoose';
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as bcrypt from 'bcryptjs';
 
 // Configuration ENV
 dotenv.config();
@@ -13,6 +16,7 @@ const connectToDB = async () => {
     const connectionUrl = process.env.REMOTEURI ? process.env.REMOTEURI : '';
     const conn = await mongoose.connect(connectionUrl);
     console.log(`Connected to Remote Host : ${conn.connection.host}`);
+    return conn;
   } catch (error) {
     console.log('Unable to Connect Remote DB', error);
   }
@@ -20,7 +24,24 @@ const connectToDB = async () => {
 
 connectToDB();
 
-app.get('/', (req, res) => res.send('Hello from server!'));
+app.get('/', async (req: Request, res: Response) => {
+  try {
+    const conn = await connectToDB();
+    // Caste to string
+    const hostname = conn?.connection.host as string;
+    const encyHost = await bcrypt.hash(hostname, 10);
+
+    return res.status(StatusCodes.OK).jsonp({
+      message: `⚡ 🖥️ Server is running here 👉 http://localhost:${PORT}`,
+      status: `${encyHost}`,
+    });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).jsonp({
+      message: '🖥️ Internal Server Error!',
+      status: false,
+    });
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`⚡Server is running here 👉 http://localhost:${PORT}`)
